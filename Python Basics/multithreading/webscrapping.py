06ab503c520010714a5ce762551bc544a999c204
itz-sidd <siddhantofficial002@gmail.com>
2025-07-20 21:50:13 +0530

    memory_management

--- Python Basics/multithreading/webscrapping.py
+++ Python Basics/multithreading/webscrapping.py
@@ -1,9 +1,62 @@
-# https://www.langchain.com/langgraph
 
-# https://blog.langchain.com/
 
-# https://python.langchain.com/docs/introduction/
+# import threading
+# import requests
+# from bs4 import BeautifulSoup
 
-import threading
-import requests
-from bs4 import BeautifulSoup
+# urls=[
+# 'https://www.langchain.com/langgraph',
+
+# 'https://blog.langchain.com/',
+
+# 'https://python.langchain.com/docs/introduction/',
+# ]
+
+
+# def fetch_content(url):
+#     response=requests.get(url)
+#     soup=BeautifulSoup(response.content,'html.parser')
+#     print(f'fetch {(len(soup.text))} charcters from {url}')
+
+# threads=[]
+
+# for url in urls:
+#     thread=threading.Thread(target=fetch_content,args=(url,))
+#     threads.append(thread)
+#     thread.start()
+
+# for thread in threads:
+#     thread.join()
+
+# print("All web-pages fetched")
+
+
+###Real World example
+
+import multiprocessing
+import math
+import sys
+import time
+
+sys.set_int_max_str_digits(100000)
+
+#Function to compute factorial of s given number
+
+def computer_factorial(number):
+    print(f"Computing fsctorial of {number}")
+    result=math.factorial(number)
+    return result
+
+if __name__=="__main__":
+    numbers=[5000,6000,7000,8000]
+
+    start_time=time.time()
+
+    #Creating a pool of work process
+    with multiprocessing.Pool() as pool:
+        results=pool.map(computer_factorial,numbers)
+
+    end_time=time.time()
+
+    print(f"Results: {results}")
+    print(f"Time Takem: {end_time - start_time} seconds")
